a5a0d2738adabd012136637ac264fd71b0ce742f
thike33 <teke3business@gmail.com>
2024-06-19 23:36:44 +0900

    掲示板一覧機能の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :board, dependent: :destroy
+
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
