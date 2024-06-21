7ce980a6ec23f2e966ee207c2f898e95d9983ba7
thike33 <teke3business@gmail.com>
2024-06-21 11:50:31 +0900

    掲示板の画像アップロード機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_19_112219) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_21_014049) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_19_112219) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "board_image"
     t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
